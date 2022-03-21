ca72e3a33297cfd950c644a38d751cf017030af8
Langson <langson.zhang@mail.utoronto.ca>
2022-03-21 08:57:23 -0400

    update

--- src/ServerScriptService/Entities/NPC/Mob/HostileMob/HostileMobTest.server.lua
+++
@@ -1,9 +0,0 @@
-local super = require(script.Parent)
-
-local start = os.clock()
-
-local dist = game.Workspace.ItemSpawner.Position - game.Workspace.SpawnLocation.Position
-
-local stop = os.clock()
-
-print(script.Name.." delta: "..stop-start)
